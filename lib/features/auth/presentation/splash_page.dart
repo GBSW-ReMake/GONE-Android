1a306af8415b2a266bd8204dd51e3856b4e5ed17
kimeunchan <kec4489@naver.com>
2026-08-17 15:20:16 +0900

    fix: 스플래시 로고 전환 자연스럽게 조정 (#13)

--- lib/features/auth/presentation/splash_page.dart
+++ lib/features/auth/presentation/splash_page.dart
@@ -22,7 +22,7 @@ class _SplashPageState extends State<SplashPage>
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 520),
+      duration: const Duration(milliseconds: 360),
     );
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await Future<void>.delayed(const Duration(milliseconds: 550));
@@ -51,10 +51,7 @@ class _SplashPageState extends State<SplashPage>
       body: Center(
         child: FadeTransition(
           opacity: Tween<double>(begin: 1, end: 0).animate(_controller),
-          child: ScaleTransition(
-            scale: Tween<double>(begin: 1, end: 1.06).animate(_controller),
-            child: const GoneLogo(width: 270),
-          ),
+          child: const GoneLogo(width: 128),
         ),
       ),
     );
